159f2c8c37dae88396e354b465083b2368ffcfcf
dttrue <danieltorres@pursuit.org>
2024-02-19 18:49:59 -0500

    Daniels express-setup

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 // You should import the express app and listen on the correct port in server.js.
+const express = require('express');
+const app = require('./index');
+
+
+
+const app = express();
+const port = 3000;
+app.get('/', index);
+app.get('/about', about);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+
+
+export default app;
