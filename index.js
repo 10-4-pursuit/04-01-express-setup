d9f3c5f8b6f080121dcfbc560fb0b9a3c9b6d2c6
KyronGreen6 <kyrongreen@pursuit.org>
2024-03-25 22:52:53 -0400

    Completed Express Setup exercise

--- index.js
+++ index.js
@@ -3,3 +3,18 @@
 // Note: You do not have to call app.listen() in this file. The tests will do that for you.
 
 // Make sure you export the express app you created here by doing module.exports = <your express app>;
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res, next) => {
+  res.send('Welcome to my first Express server!');
+});
+
+app.get('/about', (req, res, next) => {
+    res.json({
+        name: 'Sally', age: 34,
+    })
+})
+
+module.exports = app;
